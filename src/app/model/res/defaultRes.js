8bb44b1753fce30b903b51848a397a481346e8a4
bghgu <ektmf1993@gmail.com>
2018-09-13 22:24:41 +0900

    folder clean

--- src/app/model/res/defaultRes.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    new : function(body) {
-        let newUser = {
-            email: body.email,
-            password: hash.encoding(body.password)
-        };
-        return newUser;
-    }
-}
